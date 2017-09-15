d67cbc660b0a1916b84752cdc89417acd4d14d1d
Cameron Piel <cameron.piel@gitty.xyz>
2017-09-15 15:42:20 -0500

    Accept and check pin codes

--- app/views.py
+++ app/views.py
@@ -1,9 +1,26 @@
 from app import app
-from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.voice_response import VoiceResponse, Gather
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello():
 	resp = VoiceResponse()
-	resp.say("You will die in seven days!",voice='woman')
+	g = Gather(numDigits=4, action="/handle-key", method="POST")
+	g.say(voice='woman', "Please enter your assinged pin code.")
+	resp.append(g)
 	return str(resp)
 
+@app.route('/handle-key', methods=['GET', 'POST'])
+def handle_key():
+	"""Handles keys presses by the caller"""
+	resp = VoiceResponse()
+	# Get the digits pressed by the user
+	digits_pressed = request.values.get('Digits', None)
+	# Admin number
+	if digits_pressed == '2158':
+		resp.say(voice='woman', 'Hello Cameron, have a wonderful day!')
+	elif digits_pressed == '8008':
+		resp.say(voice='woman', 'Cameron will be expecting you shortly.')
+	else:
+		resp.say(voice='woman', 'Unauthorized visitor.')
+
+	return str(resp)
